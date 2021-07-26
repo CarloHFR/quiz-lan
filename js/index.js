d22d1670c4955962f7c9b4f273fad34d8ee1aeff
CarloHFR <carlohenrique78@gmail.com>
2021-07-26 16:10:23 -0300

    add sounds, main functionality finished

--- js/index.js
+++ js/index.js
@@ -1,62 +1,117 @@
-const current_question = questions.q1;
+var score = 0;
+var current_question_id = 1;
+var current_question = questions[current_question_id];
+var num_of_questions = Object.keys(questions).length;
 
-var test = function()
+
+var start_game = function()
 {
-    console.log("Test");
+    $('.modal-content').fadeOut();
+
+    setTimeout(function()
+    {
+        write_question_screen();
+        $('.modal-content').show();
+    }, 1000);
+}
+
+
+var restart_game = function()
+{
+    $('.modal-content').fadeOut();
+
+    score = 0;
+    current_question_id = 1;
+    current_question = questions[current_question_id];
+
+    setTimeout(function()
+    {
+        write_question_screen();
+        $('.modal-content').show();
+    }, 1000);
+}
+
+
+var get_next_question = function()
+{
+    setTimeout(function()
+    {
+        if(current_question_id < num_of_questions)
+        {
+            current_question_id += 1;
+            current_question = questions[current_question_id];
+            write_question_screen();
+            $('.modal-content').show();
+        }else
+        {
+            //end of game
+            write_final_screen();
+            $('.modal-content').show();
+        }     
+    }, 1000);
 }
 
 
 var check_answer = function(answer)
 {
     var ans = $(answer).val();
-    console.log(ans);
-    if (ans != current_question.correct_answer)
-        return 'INCORRECT';
-    else 
-        return 'CORRECT';
+
+    $('.modal-content').fadeOut();
+
+    if(ans == current_question.correct_answer)
+    {
+        score += 1;
+        document.getElementById('success').play();
+    }else
+    {
+        document.getElementById('fail').play();
+    }
+
+    get_next_question();
 }; 
 
 
-$(function(){
+var write_question_screen = function()
+{
+    modal = "<div class='modal-header'><h3 class='col-12 modal-title text-center'><span class='label label-warning' id='qid'></span>" + current_question.question + "</h3></div>";
+    modal += "<div class='modal-body'><div class='quiz' id='quiz' data-toggle='buttons'>";
 
-    function write_quiz(question)
+    if(current_question.type == "multi")
     {
-        modal = "<div class='modal-header'><h3><span class='label label-warning' id='qid'></span>" + question.question + "</h3></div>";
-        modal += "<div class='modal-body'><div class='quiz' id='quiz' data-toggle='buttons'>";
-
-        if(question.type == "multi")
-        {
-            modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + question.answers.a1 + "</button>";
-            modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + question.answers.a2 + "</button>";
-            modal += "<button class='element-animation3 btn btn-lg btn-light btn-block text-info' value='a3' onclick='check_answer(this)'>" + question.answers.a3 + "</button>";
-            modal += "<button class='element-animation4 btn btn-lg btn-light btn-block text-info' value='a4' onclick='check_answer(this)'>" + question.answers.a4 + "</button>";
-        }else if(question.type == "vf")
-        {
-            modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + question.answers.a1 + "</button>";
-            modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + question.answers.a2 + "</button>";
-        }
-
-        modal += "</div></div><div class='modal-footer text-muted'><span id='answer'></span>";
-
-        //Limpando a questão antiga e escrevendo a nova
-        $(".modal-content").html("");
-        $(".modal-content").html(modal);
-    }; 
-
-
-    $("label.btn").on('click',function () 
+        modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + current_question.answers.a1 + "</button>";
+        modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + current_question.answers.a2 + "</button>";
+        modal += "<button class='element-animation3 btn btn-lg btn-light btn-block text-info' value='a3' onclick='check_answer(this)'>" + current_question.answers.a3 + "</button>";
+        modal += "<button class='element-animation4 btn btn-lg btn-light btn-block text-info' value='a4' onclick='check_answer(this)'>" + current_question.answers.a4 + "</button>";
+    }else if(current_question.type == "vf")
     {
-    	var choice = $(this).find('input:radio').val();
-    	$('#quiz').fadeOut();
+        modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + current_question.answers.a1 + "</button>";
+        modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + current_question.answers.a2 + "</button>";
+    }
 
-    	setTimeout(function()
-        {
-            $("#answer").html($(this).check_answer(choice));      
-            $('#quiz').show();
-           /* something else */
-    	}, 500);
-    });    
+    modal += "</div></div><div class='modal-footer'><span class='score'>Score: " + score + "</span>";
+
+    $(".modal-content").html("");
+    $(".modal-content").html(modal);
+}; 
 
 
-    write_quiz(current_question);
-});
+var write_final_screen = function()
+{
+    if(score < 5)
+    {
+        final_message = "Que pena, você precisa estudar mais!";
+    }else if (score >= 5 && score <= 7)
+    {
+        final_message = "Parabéns, mas continue estudando!";
+    }else if (score >= 6)
+    {
+        final_message = "Parabéns, você conhece bem o assunto!";
+    }
+
+    modal = "<div class='modal-header'><h3 class='col-12 modal-title text-center'><span class='label label-warning' id='qid'></span>SCORE</h3></div>";
+    modal += "<div class='modal-body'><div class='final-score'>" + score + "</div></br><div class='final-message'>" + final_message + "</div></br><button class='element-animation1 btn btn-lg btn-light text-info' onclick='restart_game()'>Jogar Novamente</button></div>";
+    modal += "<div class='modal-footer text-muted'><span id='answer'></span>";
+
+    $(".modal-content").html("");
+    $(".modal-content").html(modal);
+}; 
--- js/questions.js
+++ js/questions.js
@@ -1,5 +1,5 @@
 questions = {
-    "q1": {
+    "1": {
         "question": "What is the scientific name of a butterfly?",
         "type": "multi",
         "answers": {
@@ -10,7 +10,87 @@ questions = {
         },
         "correct_answer": "a3"
     },
-    "q2": {
+    "2": {
+        "question": "How hot is the surface of the sun?",
+        "type": "vf",
+        "answers": {
+            "a1": "1,233 K",
+            "a2": "5,778 K"
+        },
+        "correct_answer": "a1"
+    },
+    "3": {
+        "question": "What is the scientific name of a butterfly?",
+        "type": "multi",
+        "answers": {
+            "a1": "Apis",
+            "a2": "Coleoptera",
+            "a3": "Formicidae",
+            "a4": "Rhopalocera"
+        },
+        "correct_answer": "a3"
+    },
+    "4": {
+        "question": "How hot is the surface of the sun?",
+        "type": "vf",
+        "answers": {
+            "a1": "1,233 K",
+            "a2": "5,778 K"
+        },
+        "correct_answer": "a1"
+    },
+    "5": {
+        "question": "What is the scientific name of a butterfly?",
+        "type": "multi",
+        "answers": {
+            "a1": "Apis",
+            "a2": "Coleoptera",
+            "a3": "Formicidae",
+            "a4": "Rhopalocera"
+        },
+        "correct_answer": "a3"
+    },
+    "6": {
+        "question": "How hot is the surface of the sun?",
+        "type": "vf",
+        "answers": {
+            "a1": "1,233 K",
+            "a2": "5,778 K"
+        },
+        "correct_answer": "a1"
+    },
+    "7": {
+        "question": "What is the scientific name of a butterfly?",
+        "type": "multi",
+        "answers": {
+            "a1": "Apis",
+            "a2": "Coleoptera",
+            "a3": "Formicidae",
+            "a4": "Rhopalocera"
+        },
+        "correct_answer": "a3"
+    },
+    "8": {
+        "question": "How hot is the surface of the sun?",
+        "type": "vf",
+        "answers": {
+            "a1": "1,233 K",
+            "a2": "5,778 K"
+        },
+        "correct_answer": "a1"
+    },
+    "9": {
+        "question": "What is the scientific name of a butterfly?",
+        "type": "multi",
+        "answers": {
+            "a1": "Apis",
+            "a2": "Coleoptera",
+            "a3": "Formicidae",
+            "a4": "Rhopalocera"
+        },
+        "correct_answer": "a3"
+    },
+    "10": {
         "question": "How hot is the surface of the sun?",
         "type": "vf",
         "answers": {
